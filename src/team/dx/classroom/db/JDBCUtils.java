9384a988a52c3b2ed9e0e8accdfb76210550efbb
dengjili <dengjili@outlook.com>
2015-10-08 21:13:31 +0800

    我添加了一个readme.txt文件，修改user类，删除了person类

--- src/team/dx/classroom/db/JDBCUtils.java
+++ src/team/dx/classroom/db/JDBCUtils.java
@@ -42,7 +42,6 @@ public class JDBCUtils {
 				connection.close();
 			}
 		} catch (SQLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
--- src/team/dx/classroom/domain/User.java
+++ src/team/dx/classroom/domain/User.java
@@ -1,22 +1,76 @@
 package team.dx.classroom.domain;
 
+import java.util.Date;
+
 public class User {
 
 	private String id;
 	private String nick;
 	private String email;
 	private String password;
+	private String name;
+	private String gender;
+	private Date bitthday;
+	private String address;
+	private String description;
 	private Role role; /* 角色，一个账号在系统中的角色类型 */
 
 	public User() {
 	}
 
-	public User(String id, String nick, String email, String password, Role role) {
+	public User(String id, String nick, String email, String password,
+			String name, String gender, Date bitthday, String address,
+			String description, Role role) {
 		this.id = id;
 		this.nick = nick;
 		this.email = email;
 		this.password = password;
+		this.name = name;
+		this.gender = gender;
+		this.bitthday = bitthday;
+		this.address = address;
+		this.description = description;
 		this.role = role;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getBitthday() {
+		return bitthday;
+	}
+
+	public void setBitthday(Date bitthday) {
+		this.bitthday = bitthday;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
 	public String getId() {
@@ -34,7 +88,7 @@ public class User {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
@@ -61,7 +115,10 @@ public class User {
 
 	@Override
 	public String toString() {
-		return "User [id=" + id + ", nick=" + nick + ", password=" + password + ", role=" + role + "]";
+		return "User [id=" + id + ", nick=" + nick + ", email=" + email
+				+ ", password=" + password + ", name=" + name + ", gender="
+				+ gender + ", bitthday=" + bitthday + ", address=" + address
+				+ ", description=" + description + ", role=" + role + "]";
 	}
 
 }
